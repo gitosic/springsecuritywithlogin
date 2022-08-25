af1e9e07af4fbd070c90848e48dfd559f18bf662
Vitaliy <vitaliu@yandex.ru>
2022-08-26 00:58:44 +0300

    Working version: Spring Security with Login (Spring Boot + Spring Security + MySQL)

---
+++ src/main/java/com/example/vitoject/VitojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.vitoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitojectApplication.class, args);
+	}
+
+}
